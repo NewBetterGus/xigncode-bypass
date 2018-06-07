8cc6a4d50bae9f8f8197a89911f934c7f8988f7d
Caali <caali-tera@outlook.com>
2018-06-07 22:32:20 +0200

    Add auto-removal of xhunter1.sys.

--- index.js
+++ index.js
@@ -54,3 +54,15 @@ function HandleRemovedProcess(pid)
 
 ProcessListener("TERA.exe", HandleAddedProcess, HandleRemovedProcess, SCAN_INTERVAL);
 console.log("[xigncode-bypass] Ready, waiting for game client to be started!");
+
+try
+{
+    fs.unlinkSync("C:\\Windows\\xhunter1.sys");
+    console.log("[xigncode-bypass] Traces of a previous xigncode installation have been located and removed from your system!");
+    console.log("[xigncode-bypass] Note that some registry keys might still remain on your system.");
+    console.log("[xigncode-bypass] Check out #proxy-changelog in https://discord.gg/maqBmJV for instructions on manual removal.")
+}
+catch(e)
+{
+    // Ignore errors...
+}
